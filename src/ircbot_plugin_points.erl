f2ef7a47df8dc119f1961be940ae813a0c8e0681
joekinley <rafael@tweep.de>
2015-02-10 22:08:23 +0100

    ADD: transferpoints command

--- src/ircbot_plugin_points.erl
+++ src/ircbot_plugin_points.erl
@@ -4,7 +4,8 @@
 -behaviour(gen_event).
 -export([init/1, handle_event/2, terminate/2, handle_call/2, handle_info/2, code_change/3]).
 
--define(ADMINS, ["thegypsyknight","joekinley"]).
+-define(SELF, "cassadeey").
+-define(ADMINS, ["thegypsyknight","joekinley",?SELF]).
 
 init(_Args) ->
     %ets:new(points,[set,named_table]),
@@ -29,10 +30,12 @@ handle_event(Msg, State) ->
 handle_command(_Sender, Msg) ->
   [Cmd|Parts] = string:tokens(Msg, " "),
   case Cmd of
+    "!help" -> show_help();
     "!points" -> show_points(_Sender);
     "!showpoints" -> show_points(_Sender, Parts);
     "!addpoints" -> add_points(_Sender, Parts);
-    "!removepoints" -> remove_points(string:to_lower(_Sender), Parts);
+    "!removepoints" -> remove_points(_Sender, Parts);
+    "!transferpoints" -> transfer_points(_Sender, Parts);
     "!highscore" -> show_highscore();
     _ -> ok
   end.
@@ -45,6 +48,7 @@ show_points(_Sender) ->
     _ -> ok
   end.
 
+show_points(_Sender, []) -> ok;
 show_points(_Sender, _Parts) ->
   IsAdmin = lists:member(_Sender, ?ADMINS),
   [_Nick|_] = _Parts,
@@ -53,6 +57,7 @@ show_points(_Sender, _Parts) ->
     _ -> ok
   end.
 
+add_points(_Sender, []) -> ok;
 add_points(_Sender, Parts) ->
   IsAdmin = lists:member(_Sender, ?ADMINS),
   [_Nick|[_Points|_]] = Parts,
@@ -69,6 +74,7 @@ add_points(_Sender, Parts) ->
     _ -> ok
   end.
 
+remove_points(_Sender, []) -> ok;
 remove_points(_Sender, Parts) ->
   IsAdmin = lists:member(_Sender, ?ADMINS),
   [_Nick|[_Points|_]] = Parts,
@@ -85,6 +91,15 @@ remove_points(_Sender, Parts) ->
     _ -> ok
   end.
 
+transfer_points(_Sender, Parts) when length(Parts) < 3 -> ok;
+transfer_points(_Sender, Parts) ->
+  [_Nick|[_Receiver|[_Points|_]]] = Parts,
+  case ets:lookup(points, _Nick) of
+    [{_Nick, NPoints}|_] when NPoints >= _Points -> remove_points(?SELF,[_Nick,_Points]),
+                                                    add_points(?SELF,[_Receiver,_Points]);
+    _ -> ok
+  end.
+
 show_highscore() ->
   People = lists:sublist(lists:reverse(lists:keysort(2,ets:tab2list(points))),10),
   "Top 10: "++format_people(People).
@@ -95,7 +110,10 @@ format_person({Name, Age}) ->
 format_people(People) ->
   string:join(lists:map(fun format_person/1, People), ", ").
 
-
+show_help() ->
+  ["!points - shows points, ",
+   "!transferpoints <nick> <points> - transfers your points to <nick>, ",
+   "!highscore - shows TOP 10, "].
 
 handle_call(_Request, State) -> {ok, ok, State}.
 handle_info(_Info, State) -> {ok, State}.
