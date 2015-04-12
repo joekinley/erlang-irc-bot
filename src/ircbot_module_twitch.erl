923287400c7b4e935a06ba4501edb20b94097b9f
joekinley <rafael@tweep.de>
2015-04-12 20:36:04 +0200

    CHG: Stuff

--- src/ircbot_module_twitch.erl
+++ src/ircbot_module_twitch.erl
@@ -1,7 +1,7 @@
 -module(ircbot_module_twitch).
 -author("rafael@tweep.de").
 
--define(GETMODS, "https://api.twitch.tv/kraken/channels/thegypsyknight/editors")
+%-define(GETMODS, "https://api.twitch.tv/kraken/channels/thegypsyknight/editors")
 
 start(Token) ->
   receive
